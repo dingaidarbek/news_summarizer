b877c0b9375e040c02022fea420910bed4b61ba1
Dimash <dingaidarbek@gmail.com>
2025-05-05 23:00:36 +0400

    added url

--- summarizer.py
+++ summarizer.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
+from newspaper import Article
 
 app = Flask(__name__)
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
@@ -7,13 +8,27 @@ summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 @app.route('/summarize', methods=['POST'])
 def summarize():
     data = request.get_json()
-    text = data.get('text', '')
+    url = data.get('url', '')
 
-    if not text:
-        return jsonify({'error': 'No text provided'}), 400
+    if not url:
+        return jsonify({'error': 'No URL provided'}), 400
 
-    summary = summarizer(text, max_length=100, min_length=25, do_sample=False)
-    return jsonify({'summary': summary[0]['summary_text']})
+    try:
+        article = Article(url)
+        article.download()
+        article.parse()
+        text = article.text
+
+        if not text or len(text.split()) < 50:
+            return jsonify({'error': 'Article too short or failed to parse'}), 400
+
+        summary = summarizer(text, max_length=100, min_length=25, do_sample=False)
+        return jsonify({
+            'title': article.title,
+            'summary': summary[0]['summary_text']
+        })
+    except Exception as e:
+        return jsonify({'error': f'Failed to process article: {str(e)}'}), 500
 
 if __name__ == '__main__':
     import os
